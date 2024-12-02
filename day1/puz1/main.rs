d8279b0525c7b5dec893be4e2553b4e5719d0801
Toby <tbyc11@proton.me>
2024-12-03 07:16:59 +0800

    refactor

--- day1/puz1/main.rs
+++ day1/puz1/main.rs
@@ -3,35 +3,29 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Should have been able to read the file");
     let lines = input.split("\n").collect::<Vec<&str>>();
-    let mut pair_a = Vec::new();
-    let mut pair_b = Vec::new();
+    let mut left = Vec::new();
+    let mut right = Vec::new();
     for line in lines {
         let l = line.split("   ").collect::<Vec<&str>>();
         if (l.len() != 2) {
             continue;
         }
 
-        let a = l[0].parse::<i32>();
-        let b = l[1].parse::<i32>();
-        match a {
-            Ok(n) => pair_a.push(n),
-            Err(e) => println!("{}", e),
-        }
-        match b {
-            Ok(n) => pair_b.push(n),
-            Err(e) => println!("{}", e),
-        }
+        let a = l[0].parse::<i32>().expect("Should be an integer");
+        let b = l[1].parse::<i32>().expect("Should be an integer");
+        left.push(a);
+        right.push(b)
     }
 
     // sort each column
-    pair_a.sort_unstable();
-    pair_b.sort_unstable();
+    left.sort_unstable();
+    right.sort_unstable();
 
     let mut total = 0;
 
     // compare each pair
-    for index in 0..pair_a.len() {
-        let dist = pair_a[index] - pair_b[index];
+    for index in 0..left.len() {
+        let dist = left[index] - right[index];
         total += if dist > 0 { dist } else { -dist }
     }
     println!("Total distance: {}\n", total)
