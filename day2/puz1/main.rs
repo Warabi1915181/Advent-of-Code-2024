c1ddde3bf2e75789d61f85dae55992fb3caf6037
Toby <tbyc11@proton.me>
2024-12-07 21:11:57 +0800

    refactor

--- day2/puz1/main.rs
+++ day2/puz1/main.rs
@@ -1,32 +1,43 @@
 use std::fs;
 
-fn is_safe(mut increasing: Option<bool>, prev: Option<i32>, cur: i32) -> (Option<bool>, bool) {
-    // return true if the difference is within the accepted boundary
-    fn check_diff(increasing: bool, diff: i32) -> bool {
-        if increasing {
-            (1..=3).contains(&diff)
-        } else {
-            (-3..=-1).contains(&diff)
-        }
+fn within_range(increasing: bool, diff: i32) -> bool {
+    if increasing {
+        (1..=3).contains(&diff)
+    } else {
+        (-3..=-1).contains(&diff)
     }
+}
 
-    match prev {
-        Some(n) => {
-            let diff = cur - n;
-            match increasing {
-                Some(_increasing) => (increasing, check_diff(_increasing, diff)),
-                None => {
-                    if diff == 0 {
-                        (increasing, false)
-                    } else {
-                        increasing = Some(diff > 0);
-                        (increasing, check_diff(increasing.unwrap(), diff))
+fn is_safe(levels: Vec<i32>) -> bool {
+    let mut prev: Option<i32> = None;
+    let mut increasing: Option<bool> = None;
+    let mut safe = true;
+    for level in levels.iter() {
+        let cur = *level;
+        safe = match prev {
+            Some(n) => {
+                let diff = cur - n;
+                match increasing {
+                    Some(_increasing) => within_range(_increasing, diff),
+                    None => {
+                        if diff == 0 {
+                            false
+                        } else {
+                            increasing = Some(diff > 0);
+                            within_range(increasing.unwrap(), diff)
+                        }
                     }
                 }
             }
+            None => true,
+        };
+        prev = Some(cur);
+        if !safe {
+            return false;
         }
-        None => (increasing, true),
     }
+
+    safe
 }
 
 fn main() {
@@ -34,26 +45,14 @@ fn main() {
     let lines = input.split('\n').collect::<Vec<&str>>();
     let mut num_safe_reports = 0;
     for line in lines {
-        let _levels = line.split(' ').collect::<Vec<&str>>();
-        let levels_num = _levels.iter().map(|n| n.parse::<i32>());
-        let levels: Vec<i32> = match levels_num.collect() {
+        let levels_str = line.split(' ').collect::<Vec<&str>>();
+        let levels: Vec<i32> = match levels_str.iter().map(|n| n.parse::<i32>()).collect() {
             Ok(levels) => levels,
             Err(_) => {
                 continue;
             }
         };
-
-        let mut prev: Option<i32> = None;
-        let mut increasing: Option<bool> = None;
-        let mut safe = true;
-        for level in levels.iter() {
-            let cur = *level;
-            let _safe;
-            (increasing, _safe) = is_safe(increasing, prev, cur);
-            safe = safe && _safe;
-            prev = Some(cur);
-        }
-        num_safe_reports += safe as i32;
+        num_safe_reports += is_safe(levels) as i32;
     }
     println!("Number of safe reports: {}", num_safe_reports);
 }
